9cad6140d83bae8f4d25f2b009beed833b34bf89
Everton da Silva Serafim <thi.ger@hotmail.com>
2020-12-19 17:18:35 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/everton/cursomc/service/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.everton.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.everton.cursomc.domain.Categoria;
+import com.everton.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+		
+	}
+}
