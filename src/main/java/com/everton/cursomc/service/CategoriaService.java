b23e39a33ca7fe91a889f3b4f34e607ea3fed221
Everton da Silva Serafim <thi.ger@hotmail.com>
2020-12-21 17:46:45 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/everton/cursomc/service/CategoriaService.java
+++ src/main/java/com/everton/cursomc/service/CategoriaService.java
@@ -12,7 +12,7 @@ public class CategoriaService {
 	
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id) {
+	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElse(null);
 		
