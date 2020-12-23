2c6828632dd54e2b167f57d2e339910b96ad87c2
Everton da Silva Serafim <thi.ger@hotmail.com>
2020-12-23 18:31:41 -0300

    Ajustes no endpoint /cartegorias/{id}

--- src/main/java/com/everton/cursomc/service/CategoriaService.java
+++ src/main/java/com/everton/cursomc/service/CategoriaService.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Service;
 import com.everton.cursomc.domain.Categoria;
 import com.everton.cursomc.repositories.CategoriaRepository;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @Service
 public class CategoriaService {
 	
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 		
 	}
