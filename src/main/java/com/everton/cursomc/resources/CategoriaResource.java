64e77c445b72dfc7bde8362be368173ed2f48b2d
Everton da Silva Serafim <thi.ger@hotmail.com>
2021-01-02 18:13:45 -0300

    Estado e Cidade adicionados

--- src/main/java/com/everton/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/everton/cursomc/resources/CategoriaResource.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.everton.cursomc.domain.Categoria;
 import com.everton.cursomc.service.CategoriaService;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @RestController
 @RequestMapping(value = "/categorias")
 
@@ -20,7 +22,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find (@PathVariable Integer id) {
+	public ResponseEntity<?> find (@PathVariable Integer id) throws ObjectNotFoundException {
 		
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
